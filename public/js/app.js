8897f9ceb998c9d32ca598c9acda728aee15bb4d
Shanaun <sclayton1183@gmail.com>
2018-11-16 18:24:51 -0800

    updated app.js and index.html

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+      <h1>Presents</h1>
+      <Presents/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App></App>,
+  document.querySelector('container')
+)
