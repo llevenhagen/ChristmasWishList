bcc1598a92ecbf6794df3531395364bf2409d5fa
Shanaun <sclayton1183@gmail.com>
2018-11-17 18:41:12 -0800

    fixing create and update routes

--- app/controllers/presents_controller.rb
+++ app/controllers/presents_controller.rb
@@ -1,27 +1,27 @@
 class PresentsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  # get index (all)
+  # get index 
   def index
     render json: Present.all
   end
 
-  # get one (by id)
+  # get
   def show
     render json: Present.find(params["id"])
   end
 
-  # create one
+  # create
   def create
     render json: Present.create(params["present"])
   end
 
-  # delete one (by id)
+  # delete
   def delete
     render json: Present.delete(params["id"])
   end
 
-  # update one (by id)
+  # update
   def update
     render json: Present.update(params["id"], params["present"])
   end
--- app/models/present.rb
+++ app/models/present.rb
@@ -1,71 +1,50 @@
 class Present
-    # ==================================================
-    #                      SET UP
-    # ==================================================
-    # connect to postgres
-    if(ENV['DATABASE_URL'])
-        uri = URI.parse(ENV['DATABASE_URL'])
-        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
-    else
-        DB = PG.connect(host: "localhost", port: 5432, dbname: 'ChristmasWishList_development')
+  # ==================================================
+  #                      SET UP
+  # ==================================================
+  # connect to postgres
+  if(ENV['DATABASE_URL'])
+    uri = URI.parse(ENV['DATABASE_URL'])
+    DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+  else
+    DB = PG.connect(host: "localhost", port: 5432, dbname: 'ChristmasWishList_development')
+  end
+  #the rest of your code goes here...
+  # ==================================================
+  #                      ROUTES
+  # ==================================================
+  # get all
+  def self.all
+    results = DB.exec("SELECT * FROM presents;")
+    return results.map do |result|
+      {
+        "id" => result["id"].to_i,
+        "name" => result["name"],
+        "image" => result["image"],
+        "price" => result["price"],
+        "bought_status" => result["bought_status"]
+      }
     end
-    #the rest of your code goes here...
-    # ==================================================
-    #                      ROUTES
-    # ==================================================
-    # get all
-   def self.all
-         results = DB.exec("SELECT * FROM presents;")
-         return results.map do |result|
-             {
-                 "id" => result["id"].to_i,
-                 "name" => result["name"],
-                 "image" => result["image"],
-                 "price" => result["price"],
-                 "bought_status" => result["bought_status"]
-             }
-         end
-   end
-    # get one by id
-    def self.find(id)
+  end
+
+  # get
+  def self.find(id)
     results = DB.exec("SELECT * FROM presents WHERE id=#{id};")
     return {
-        "id" => results.first["id"].to_i,
-        "name" => results.first["name"],
-        "image" => results.first["image"],
-        "price" => results.first["price"],
-        "bought_status" => results.first["bought_status"]
+      "id" => results.first["id"].to_i,
+      "name" => results.first["name"],
+      "image" => results.first["image"],
+      "price" => results.first["price"],
+      "bought_status" => results.first["bought_status"]
     }
   end
-    # create one
-      def self.create(opts)
-        results = DB.exec(
-     <<-SQL
-         INSERT INTO presents (name, image, price, bought_status)
-         VALUES ( '#{opts["name"]}', '#{opts["image"]}', '#{opts["price"]}', #{opts["bought_status"]} )
-         RETURNING id, name, image, price, bought_status;
-     SQL
- )
- return {
-        "id" => results.first["id"].to_i,
-        "name" => results.first["name"],
-        "image" => results.first["image"],
-        "price" => results.first["price"],
-        "bought_status" => results.first["bought_status"]
-    }
-    end
-    # delete one (by id)
-    def self.delete(id)
-      results = DB.exec("DELETE FROM presents WHERE id=#{id};")
-      return { "deleted" => true }
-    end
-    # update one (by id)
-    def self.update(id, opts)
+
+  # create
+  def self.create(opts)
       results = DB.exec(
           <<-SQL
-              UPDATE presents
-              SET name='#{opts["name"]}', image='#{opts["image"]}', price='#{opts["price"]}', bought_status='#{opts["bought_status"]}
-              WHERE id=#{id}
+              INSERT INTO presents (name, image, price, bought_status)
+              VALUES ( '#{opts["name"]}', '#{opts["image"]}', '#{opts["price"]}', #{opts["bought_status"]} )
               RETURNING id, name, image, price, bought_status;
           SQL
       )
@@ -76,5 +55,31 @@ class Present
           "price" => results.first["price"],
           "bought_status" => results.first["bought_status"]
       }
-   end
+  end
+
+ # delete
+  def self.delete(id)
+    results = DB.exec("DELETE FROM presents WHERE id=#{id};")
+    return { "deleted" => true }
+  end
+
+ #update
+  def self.update(id, opts)
+    results = DB.exec(
+      <<-SQL
+        UPDATE presents
+        SET name='#{opts["name"]}', image='#{opts["image"]}', price='#{opts["price"]}', bought_status=#{opts["bought_status"]}
+        WHERE id=#{id}
+        RETURNING id, name, image, price, bought_status;
+      SQL
+    )
+    return {
+      "id" => results.first["id"].to_i,
+      "name" => results.first["name"],
+      "image" => results.first["image"],
+      "price" => results.first["price"],
+      "bought_status" => results.first["bought_status"]
+    }
+  end
+
 end
