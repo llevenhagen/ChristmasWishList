c0330c4cf7a626dd98099686c8515d21fcfb8ccb
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2018-11-18 12:35:57 -0800

    fixed togglestate for edit present form on show page

--- public/js/Present.js
+++ public/js/Present.js
@@ -1,6 +1,17 @@
 class Present extends React.Component {
   constructor(props){
     super(props)
+    this.toggleState= this.toggleState.bind(this)
+    this.state ={
+      editPresentIsVisible: false
+    }
+  }
+  toggleState(st1){
+    this.setState(
+      {
+        [st1]: !this.state[st1]
+      }
+    )
   }
   render(){
     return (
@@ -13,14 +24,18 @@ class Present extends React.Component {
             {this.props.present.name}</h4>
             <h4><span>Price:</span>
             {this.props.present.price}</h4>
-            <h4><span>Name:</span>
+            <h4><span>Bought Status:</span>
             {this.props.present.bought_status}</h4>
           </div>
           <button className="button"
                 onClick={ ()=> this.props.toggleState('presentListIsVisible', 'presentIsVisible')}>Back to Present List
           </button>
+          <button onClick={()=> this.toggleState('editPresentIsVisible')}>Edit present</button>
+          {
+            this.state.editPresentIsVisible ?
           <PresentForm present={this.props.present}
-          handleSubmit={this.props.handleSubmit}/>
+          handleSubmit={this.props.handleSubmit}/> : ''
+        }
       </div>
     )
   }
