5eb56919a49f6b5a35dcc1498e2f6fbc485290ba
Shanaun <sclayton1183@gmail.com>
2018-11-18 19:05:25 -0800

    changes

--- public/js/PresentList.js
+++ public/js/PresentList.js
@@ -17,7 +17,7 @@ class PresentList extends React.Component {
                   alt={present.name}/>
               </div>
               <div className="present-name">
-              
+
                 <button className="button"
                 onClick={()=>
                   this.props.deletePresent(present, index)}>Delete</button>
